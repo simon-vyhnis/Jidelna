1c6b51b179ce99896b36b76588074de1fb037b70
Šimon Vyhnis <simon.vyhnis@gmail.com>
2021-11-21 15:29:32 +0100

    Created MainActivity menu

--- app/src/main/java/com/simon/jdelna/ui/MainActivity.java
+++ app/src/main/java/com/simon/jdelna/ui/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.simon.jdelna.R;
 import com.simon.jdelna.http.DailyMenu;
@@ -17,13 +20,14 @@ public class MainActivity extends AppCompatActivity {
     public static final String PREFERENCES_FILE = "com.simon.jidelna.LOGIN_INFO";
     TextView text;
     HttpDao http;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences preferences = getSharedPreferences(PREFERENCES_FILE,MODE_PRIVATE);
+        preferences = getSharedPreferences(PREFERENCES_FILE,MODE_PRIVATE);
         if(!preferences.contains("email")){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -34,17 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
         http.login(preferences.getString("email", "err"), preferences.getString("password", "err")).observe(this, response->{
             if(response.isSuccessful()){
-                StringBuilder builder = new StringBuilder();
-                for(User user: response.getUsers()){
-                    builder.append(user.getName()).append("\n");
-                }
-                text.setText(builder.toString());
                 loadMenu();
             }else{
-                text.setText("Přihlašování selhalo");
+                Toast.makeText(this, "Přihlašování selhalo", Toast.LENGTH_SHORT).show();
             }
         });
-
 
 
     }
@@ -64,6 +62,25 @@ public class MainActivity extends AppCompatActivity {
             text.setText(textBuilder.toString());
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.logout){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }else if(item.getItemId() == R.id.switch_user){
+            Intent intent = new Intent(this, ChooseUserActivity.class);
+            startActivity(intent);
+        }
+        return(super.onOptionsItemSelected(item));
+    }
+
 
     @Override
     public void onBackPressed() {}
